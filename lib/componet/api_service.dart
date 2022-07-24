18bd709786afebf0ec8be0ccd0fc62fefe84bb6e
ARYAN PANDEY <74139088+aryanpandey-123@users.noreply.github.com>
2022-07-24 10:58:48 +0530

    Update api_service.dart

--- lib/componet/api_service.dart
+++ lib/componet/api_service.dart
@@ -1,58 +1,45 @@
+import 'package:YourSawaal/componet/Models/login_response_model.dart';
+import 'package:YourSawaal/componet/Models/signup_response_model.dart';
+import 'package:YourSawaal/screens/signup.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:YourSawaal/componet/Models/login_request_model.dart';
-import 'package:YourSawaal/componet/Models/login_response_model.dart';
-import 'package:YourSawaal/componet/Models/signup_request_model.dart';
-import 'package:YourSawaal/componet/Models/signup_response_model.dart';
-import 'package:YourSawaal/componet/config.dart';
-import 'package:YourSawaal/componet/shared_services.dart';
-import 'package:http/http.dart' as http;
-
-class APIservice {
-  static var client = http.Client();
-  static Future<bool> login(LoginRequestModel model) async {
-    Map<String, String> requestHeaders = {
-      'Content-Type': 'application/json',
-    };
-    var url = Uri.http(Config.apiURL, Config.loginAPI);
-
-    var response = await client.post(url,
-        headers: requestHeaders, body: jsonEncode(model.toJson()));
-    if (response.statusCode == 200) {
-      await sharedservice.setLoginDetails(loginResponseJson(response.body));
+class APIService {
+  Future<bool> login(LoginRequestModel requestModel) async {
+    // const String endpoint = "https://reqres.in/api/login";
+    const String endpoint = "http://3.6.36.203/user/login";
+    final Uri url = Uri.parse(endpoint);
+    final response = await http.post(
+      url,
+      body: requestModel.toJson(),
+      // headers: <String, String>{
+      //   'Content-Type': 'application/json',
+      //   'Accept': 'application/json'
+      // }
+    );
+    print(response.body);
+    if (response.statusCode == 200 || response.statusCode == 400) {
       return true;
+      // LoginResponseModel.fromJson(
+      //   json.decode(response.body),
+      // );
     } else {
-      return false;
+      throw Exception('Failed to load data!');
     }
   }
 
-  static Future<RegisterResponseModel> register(
-      RegisterRequestModel model) async {
-    Map<String, String> requestHeaders = {
-      'Content-Type': 'application/json',
-    };
-    var url = Uri.http(Config.apiURL, Config.registerAPI);
-
-    var response = await client.post(url,
-        headers: requestHeaders, body: jsonEncode(model.toJson()));
-
-    return registerResponseModel(response.body);
-  }
-
-  static Future<String> getuserprofile() async {
-    var loginDetails = await sharedservice.loginDetails();
-
-    Map<String, String> requestHeaders = {
-      'Content-Type': 'application/json',
-      // 'Authorization': 'Basic ${loginDetails!.data.token}'
-    };
-    var url = Uri.http(Config.apiURL, Config.userprofile);
-
-    var response = await client.get(url, headers: requestHeaders);
-    if (response.statusCode == 200) {
-      return response.body;
+  Future<bool> signup(signupRequestModel requestModel) async {
+    const String endpoint = "https://reqres.in/api/login";
+    // const String endpoint = "http://3.6.36.203/user/register";
+    final Uri url = Uri.parse(endpoint);
+    final response = await http.post(url, body: requestModel.toJson());
+    if (response.statusCode == 200 || response.statusCode == 400) {
+      return true;
+      // signupResponseModel.fromJson(
+      //   json.decode(response.body),
+      // );
     } else {
-      return "";
+      throw Exception('Failed to load data!');
     }
   }
 }
